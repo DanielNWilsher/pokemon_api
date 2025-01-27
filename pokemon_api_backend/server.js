8c160932f77649c99aad00e5b2770c9be73f9b4f
Daniel Wilsher <daniel.m.wilsher@gmail.com>
2025-01-26 20:29:11 -0500

    Connected API and sent French Golduck name from backend to front end

---
+++ pokemon_api_backend/server.js
@@ -0,0 +1,38 @@
+import Pokedex from 'pokedex-promise-v2';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const P = new Pokedex();
+app.use(cors());
+
+app.get('/hello', (req, res) => {
+    res.send('Hello from the backend!');
+});
+
+async function fetchPokemonData(req, res) {
+    try {
+        const golduckSpecies = await P.getPokemonSpeciesByName("golduck");
+        const frenchName = golduckSpecies.names.filter(pokeAPIName => pokeAPIName.language.name === 'fr')[0].name;
+        res.send(frenchName);
+    } catch (error) {
+        console.error(error);
+    }
+
+    P.getPokemonByName(['eevee', 'ditto']) // with Promise
+      .then((response) => {
+        res.send(response);
+      })
+      .catch((error) => {
+          console.error(error);
+      });
+}
+
+app.get('/', (req, res) => {
+    fetchPokemonData(req, res);
+});
+
+const port = 3100;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
